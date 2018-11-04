d94ba4fed24f88660e2a684339c6b90493a66fe5
Meynor R Lucas <andre.rlucas@outlook.com>
2018-11-04 01:37:03 -0600

    v0.2.0 Rendering: Header
    Header fully rendered & a pin social bar implemented greatly! Has toggle functions from jQuery

---
+++ Traveler_Blog/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(".dropdown-trigger").dropdown();
+
+  let slide = $("[id^='slide']");
+  let slide_1 = $("#slide_1");
+  let slide_2 = $("#slide_2");
+  
+  slide.click(e => {
+    e.preventDefault();
+
+    slide_2.toggleClass('slide-right');
+    slide_1.toggleClass('social-container-right');
+  });
+});
